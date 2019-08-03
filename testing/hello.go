8a32b027b1590d0988d85b1e6b233509620864f9
Giovanny <gio96gomez@gmail.com>
2019-08-03 09:45:59 -0500

    Dojo goland

---
+++ testing/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func hello() string {
+
+	return "HELLO WORLD!!"
+
+}
+
+func main() {
+
+	fmt.Println(hello())
+
+}
---
+++ testing/hello_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	got := hello()
+	want := "HELLO WORLD!!"
+
+	if got != want {
+
+		t.Errorf("got %q want %q", got, want)
+
+	}
+
+}
